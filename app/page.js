2767d88d67c3cefed89c29e0380078a1731c372b
sathiyaibe <sathiyaibd@gmail.com>
2023-11-06 22:16:26 +0530

    added the scroll based animation to the title errore

--- app/page.js
+++ app/page.js
@@ -1,23 +1,13 @@
 'use client'
 import {  Navbar } from '../components';
 import { Services, BluePrint, Team, Faq, Hero, Contact, Footer, World } from '../sections';
-import Lenis from '@studio-freight/lenis';
 import {gsap,ScrollTrigger} from 'gsap/all';
 const Page = () => {
-  const lenis = new Lenis({
-    smooth: true,
-  })
 
-
-  
-  function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-  }
-  
-  requestAnimationFrame(raf)
 return(
-  <div>
+  <div
+  
+>
     {/* <Navbar /> */}
     <Hero /> 
       <Services />
