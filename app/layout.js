3cc5139cd0cb17ddfad336fdfb0deb497c2285f7
sathiyaibe <sathiyaibd@gmail.com>
2023-11-24 16:43:27 +0530

    pixel and google tag

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,30 @@
+'use client'
 import '../styles/globals.css';
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module';
 
-const RootLayout = ({ children }) => (
+const RootLayout = ({ children }) => {
+  useEffect(() => {
+    import('react-facebook-pixel')
+    .then((x) => x.default)
+    .then((ReactPixel) => {
+      ReactPixel.init('666898285296245') // facebookPixelId
+      ReactPixel.pageView()
+
+     
+    })
+    TagManager.initialize({ gtmId:"GTM-KM755MWP" });
+}, []);
+  return(
   <html lang="en">
     <head>
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
       <script src="https://cdn.jsdelivr.net/npm/gsap@3.12/dist/gsap.min.js"></script>
+
     </head>
     <body>{children}</body>
   </html>
 );
-
+  }
 export default RootLayout;
